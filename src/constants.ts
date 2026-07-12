80705b36f89d1fcc96f0cfb7592ea051e1b1d2f0
shezac <shezac>
2026-07-11 23:51:47 -0400

    remove unused code, and add leaf for ui

---
+++ src/constants.ts
@@ -0,0 +1,5 @@
+export const FLASHCARDS_VIEW_CONFIG = {
+    type: "flashcards-view",
+    name: "Flashcards",
+    icon: "flashcard",
+};
--- src/main.ts
+++ src/main.ts
@@ -5,86 +5,67 @@ import {
 	Modal,
 	Notice,
 	Plugin,
+	WorkspaceLeaf,
 } from 'obsidian';
 import {
 	DEFAULT_SETTINGS,
 	MyPluginSettings,
 	SampleSettingTab,
 } from './settings';
+import {
+	FLASHCARDS_VIEW_CONFIG,
+} from "./constants";
 
-// Remember to rename these classes and interfaces!
 
-export default class HelloWorldPlugin extends Plugin {
+export default class AIFlashcards extends Plugin {
 	settings!: MyPluginSettings;
 
 	async onload() {
 		await this.loadSettings();
-
-		this.addRibbonIcon('dice', 'AI Flashcards', () => {
-			new Notice('AI Flashcard!');
-		});
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status bar text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-modal-simple',
-			name: 'Open modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			},
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'replace-selected',
-			name: 'Replace selected content',
-			editorCallback: (
-				editor: Editor,
-				_ctx: MarkdownView | MarkdownFileInfo,
-			) => {
-				editor.replaceSelection('Sample editor command');
-			},
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-modal-complex',
-			name: 'Open modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView =
-					this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-				return false;
-			},
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new SampleSettingTab(this.app, this));
 
-		// // If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// // Using this function will automatically remove the event listener when this plugin is disabled.
+		this.addRibbonIcon(
+			"square-asterisk",
+			"Open AI Flashcards",
+			async () => {
+				const leafs = this.app.workspace.getLeavesOfType(
+					FLASHCARDS_VIEW_CONFIG.type
+				);
+				let leaf: WorkspaceLeaf;
+				if (leafs.length === 0) {
+					leaf =
+						this.app.workspace.getRightLeaf(false) ??
+						this.app.workspace.getLeaf();
+					await leaf.setViewState({
+						type: FLASHCARDS_VIEW_CONFIG.type,
+					});
+				} else {
+					leaf = leafs.first()!;
+				}
+				await this.app.workspace.revealLeaf(leaf);
+			}
+		);
+
+		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
+		// Using this function will automatically remove the event listener when this plugin is disabled.
 		// this.registerDomEvent(activeDocument, 'click', (_evt: MouseEvent) => {
 		// 	new Notice('Click');
 		// });
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(
-			window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000),
-		);
 	}
 
-	onunload() { }
+	async onunload() {
+		const leafs = this.app.workspace.getLeavesOfType(
+			FLASHCARDS_VIEW_CONFIG.type
+		);
+		let leaf: WorkspaceLeaf;
+		if (leafs.length === 0) {
+			return;
+		} else {
+			leaf = leafs.first()!;
+		}
+		leaf.detach();
+	}
 
 	async loadSettings() {
 		this.settings = Object.assign(
@@ -96,17 +77,5 @@ export default class HelloWorldPlugin extends Plugin {
 
 	async saveSettings() {
 		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	onOpen() {
-		const { contentEl } = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const { contentEl } = this;
-		contentEl.empty();
 	}
 }
